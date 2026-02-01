ce59bf9357fbdbceb5a35772203565552a38fc32
manij-20 <jmanikanta321@gmail.com>
2026-02-01 15:05:43 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -2,6 +2,6 @@ module "network_ec2" {
   source         = "../modules/network_ec2"
   vpc_cidr       = "10.0.0.0/16"
   subnet_cidr    = "10.0.1.0/24"
-  ami_id         = "ami-0ff5003538b60d5ec"
+  ami_id         = "ami-id"
   instance_type  = "t2.micro"
 }
